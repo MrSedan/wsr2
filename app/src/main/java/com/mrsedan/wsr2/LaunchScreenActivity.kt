259aaf6346ef49a741b6e5c471e79085a6eb6957
MrSedan <elpashevs@bk.ru>
2022-02-03 16:13:45 +0300

    Created LaunchScreenActivity

---
+++ app/src/main/java/com/mrsedan/wsr2/LaunchScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.mrsedan.wsr2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+    }
+}
